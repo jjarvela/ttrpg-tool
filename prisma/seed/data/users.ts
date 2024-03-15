66743c4276735d00821c43b436fe7a6cf61b71cc
TerlfY <141029876+TerlfY@users.noreply.github.com>
2024-03-15 10:36:20 +0200

    add mock user data

---
+++ prisma/seed/data/users.ts
@@ -0,0 +1,67 @@
+module.exports = [
+  // User 1
+  {
+    uid: "024a6844-ec00-4b0c-9e8d-1604f227d048",
+    username: "Jane",
+    password_hash:
+      "$argon2id$v=19$m=65536,t=3,p=4$DnnPcb9QqOiSjwthZ1+5jg$xla6n2awJ945pGKCTIwJbefX08hMxb4KUkvekO/jkCc",
+    email: "jane@mail.com",
+    created_at: new Date("2024-01-01T10:00:00Z"),
+    screen_name: "Test User ✨",
+    timezone: "UTC+02:00",
+    person_description: "Hi, I'm Jane. I'm a test user.",
+    profile_image: "https://randomuser.me/api/portraits/women/9.jpg",
+  },
+  // User 2
+  {
+    uid: "1e347524-16d9-4a26-b355-25b41e7a9643",
+    username: "JohnDoe",
+    password_hash:
+      "$argon2id$v=19$m=65536,t=3,p=4$9qUFDc8t/6FYQxbYQ+Gvjg$AWxChj3rY6S1yVlAZyKdLPUfDQbbk9vRXHZKk5pZg8w",
+    email: "john@mail.com",
+    created_at: new Date("2024-02-10T08:15:00Z"),
+    screen_name: "JD",
+    timezone: "UTC-05:00",
+    person_description: "Hello, I'm John. Nice to meet you!",
+    profile_image: "https://randomuser.me/api/portraits/men/11.jpg",
+  },
+  // User 3
+  {
+    uid: "3acba32d-840f-4643-9f48-8b1f6946a96c",
+    username: "EmilySmith",
+    password_hash:
+      "$argon2id$v=19$m=65536,t=3,p=4$MRyxQJh3TnNYNTM6P/rG3g$enb2NW6WyQ5FA5b5w3eJtZvImGirxBh1oHjAnEMwosE",
+    email: "emily@mail.com",
+    created_at: new Date("2024-03-05T14:30:00Z"),
+    screen_name: "Em",
+    timezone: "UTC+08:00",
+    person_description: "Hi there, I'm Emily!",
+    profile_image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  // User 4
+  {
+    uid: "b255c6b2-13a6-4a80-8916-95ad9c1d4f68",
+    username: "DavidBrown",
+    password_hash:
+      "$argon2id$v=19$m=65536,t=3,p=4$7oiPQuAfcU0Lx4v7P4mvUQ$Krl/j7jBKDqynZoi1fMzqYt2ZI3dGlt47LzB7I8nrY8",
+    email: "david@mail.com",
+    created_at: new Date("2024-04-20T12:45:00Z"),
+    screen_name: "Dave",
+    timezone: "UTC-07:00",
+    person_description: "Hey, I'm David. Nice to meet you all!",
+    profile_image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  // User 5
+  {
+    uid: "d8c6c1fe-28de-4923-b17f-1b593394a423",
+    username: "SarahJohnson",
+    password_hash:
+      "$argon2id$v=19$m=65536,t=3,p=4$YGn2APNOATyTOi1ps7/m1g$3/XShio2aLV/GF5Sbo2zQuLnFxZ49TzW0NTLbRIWrxU",
+    email: "sarah@mail.com",
+    created_at: new Date("2024-05-15T17:20:00Z"),
+    screen_name: "SJ",
+    timezone: "UTC-03:00",
+    person_description: "Hi, I'm Sarah! Excited to be here.",
+    profile_image: "https://randomuser.me/api/portraits/women/2.jpg",
+  },
+];
