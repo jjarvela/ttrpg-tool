3e2e95e55813861a80b2930c5d53f0ffefd93f75
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-03-15 14:54:11 +0200

    Edit landing page to implement new component structure

--- app/(auth)/welcome/page.tsx
+++ app/(auth)/welcome/page.tsx
@@ -2,14 +2,14 @@ import Main from "../../_components/wrappers/PageMain";
 import RowWrapper from "../../_components/wrappers/RowWrapper";
 import Hero from "./_components/Hero";
 import Introduction from "./_components/Introduction";
-import RegisterForm from "./_components/RegisterForm";
+import LandingForm from "./_components/LandingForm";
 
 export default function Landing() {
   return (
     <Main>
       <RowWrapper className="w-full border-b border-b-black50">
         <Hero />
-        <RegisterForm />
+        <LandingForm />
       </RowWrapper>
       <Introduction></Introduction>
     </Main>
