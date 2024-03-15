01c09d3544aecd409a36050ff10c5b9fe409c62c
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-03-15 14:54:22 +0200

    Create skeleton for separate login page

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,15 @@
+import ColumnWrapper from "../../_components/wrappers/ColumnWrapper";
+import Main from "../../_components/wrappers/PageMain";
+import LoginForm from "../../_components/forms/LoginForm";
+import { redirect } from "next/navigation";
+
+export default function Login() {
+  return (
+    <Main className="content-center items-center justify-center text-center">
+      <h1 className="text-accent-gradient">Welcome back!</h1>
+      <ColumnWrapper>
+        <LoginForm goToRegister={() => redirect("/welcome")} />
+      </ColumnWrapper>
+    </Main>
+  );
+}
