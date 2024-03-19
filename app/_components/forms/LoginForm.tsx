ca9933ffd3f51d51b2402e6b5288d37eb45033fe
Laura Tiilikainen <laura.tiilikainen@hotmail.com>
2024-03-19 15:56:33 +0200

    begin implementing auth logic to login and register forms

--- app/_components/forms/LoginForm.tsx
+++ app/_components/forms/LoginForm.tsx
@@ -1,10 +1,16 @@
 "use client";
-
 import { twMerge } from "tailwind-merge";
 import PasswordInput from "../inputs/PasswordInput";
 import TextInput from "../inputs/TextInput";
 import ColumnWrapper from "../wrappers/ColumnWrapper";
 import Button from "../Button";
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { LoginSchema } from "../../../schemas";
+import { useEffect } from "react";
 
 type LoginFormProps = {
   goToRegister: () => void;
@@ -18,19 +24,17 @@ export default function LoginForm({
   className,
   title,
 }: LoginFormProps) {
+  const {register, handleSubmit, formState} = useForm<z.infer<typeof LoginSchema>>();
+
   return (
     <section id="login" className={twMerge("text-center", className)}>
-      <form>
+      <form onSubmit={handleSubmit(() => {})}>
         {title && <h2 className="my-1 mb-2">Log in</h2>}
         <ColumnWrapper className="mx-auto w-[80%] gap-1 pb-1">
-          <TextInput placeholder="username" className="w-full" required />
-          <PasswordInput placeholder="password" className="w-full" required />
+          <TextInput className="w-full" placeholder="username" error={formState.errors.username?.message} {...register("username")}/>
+          <PasswordInput className="w-full" placeholder="password" error={formState.errors.password?.message} {...register("password")}/>
           <Button
             className="btn-primary"
-            onClick={(e) => {
-              e.preventDefault();
-              console.log("Login button");
-            }}
           >
             Login
           </Button>
--- app/_components/forms/RegisterForm.tsx
+++ app/_components/forms/RegisterForm.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { RegisterSchema } from "../../../schemas";
 import Button from "../Button";
 import PasswordInput from "../inputs/PasswordInput";
 import TextInput from "../inputs/TextInput";
@@ -17,15 +21,17 @@ export default function RegisterForm({
   className,
   title,
 }: RegisterFormProps) {
+  const {register, handleSubmit, formState} = useForm<z.infer<typeof RegisterSchema>>();
+  
   return (
     <section id="register" className={twMerge("text-center", className)}>
-      <form>
+      <form onSubmit={handleSubmit(() => {})}>
         {title && <h2 className="my-1 mb-2">Sign up</h2>}
         <ColumnWrapper className="mx-auto w-[80%] gap-1 pb-1">
-          <TextInput type="email" placeholder="email" className="w-full" />
-          <TextInput placeholder="username" className="w-full" />
-          <PasswordInput placeholder="password" className="w-full" />
-          <PasswordInput placeholder="confirm password" className="w-full" />
+          <TextInput type="email" placeholder="email" className="w-full" error={formState.errors.email?.message} {...register("email")}/>
+          <TextInput placeholder="username" className="w-full" error={formState.errors.username?.message} {...register("username")}/>
+          <PasswordInput placeholder="password" className="w-full" error={formState.errors.password?.message} {...register("password")}/>
+          <PasswordInput placeholder="confirm password" className="w-full" error={formState.errors.confirmPassword?.message} {...register("confirmPassword")}/>
           <Button
             className="btn-primary"
             onClick={(e) => {
