c0c2932d0083059c456d6b4dd1e73e93d935ea52
TerlfY <141029876+TerlfY@users.noreply.github.com>
2024-03-21 16:01:36 +0200

    add more styling

--- app/_components/SideMenu.tsx
+++ app/_components/SideMenu.tsx
@@ -5,14 +5,15 @@ import Image from "next/image";
 import diamond from "../../public/icons/diamond.svg";
 import pipeTop from "../../public/icons/pipe-top.svg";
 import pizzaSlice from "../../public/icons/pizza-slice.svg";
-
+import MaterialSymbolsLight3p from "../../public/icons/MaterialSymbolsLight3p";
+import MaterialSymbolsLightAdd from "../../public/icons/MaterialSymbolsLightAdd";
 interface Server {
   id: number;
   name: string;
   icon: string;
 }
 
-const SideMenu: React.FC = () => {
+const SideMenu = () => {
   const servers: Server[] = [
     { id: 1, name: "Mansen Maakarit", icon: diamond },
     { id: 2, name: "Don't Come Here", icon: pipeTop },
@@ -23,19 +24,22 @@ const SideMenu: React.FC = () => {
     <nav>
       <ColumnWrapper
         align="items-center"
-        className="absolute left-0 top-5 mb-2"
+        className="fixed left-0 top-0 mb-2 border-r-2 border-gray-600"
       >
+        <MaterialSymbolsLight3p width={40} height={40} />
         <ul>
           {servers.map((server) => (
             <li key={server.id}>
               <Link href={`/server/${server.id}`}>
                 <div className="group relative">
-                  <Image
-                    src={server.icon}
-                    alt={server.name}
-                    className="h-12 w-12 group-hover:opacity-80"
-                  />
-                  <span className="absolute bottom-2 left-20 -translate-x-1/2 transform rounded-md bg-black px-2 py-1 text-center text-white opacity-0 transition-opacity group-hover:opacity-100">
+                  <span className="relative my-2 inline-block overflow-hidden rounded-full bg-gray-500 shadow-md transition-all group-hover:rounded-md">
+                    <Image
+                      src={server.icon}
+                      alt={server.name}
+                      className="h-12 w-12 cursor-pointer rounded-full transition-all group-hover:rounded-md"
+                    />
+                  </span>
+                  <span className="absolute bottom-0 left-20 -translate-x-1/2 transform rounded-md bg-black px-2 py-1 text-center text-white opacity-0 transition-opacity group-hover:opacity-100">
                     {server.name}
                   </span>
                 </div>
@@ -43,6 +47,15 @@ const SideMenu: React.FC = () => {
             </li>
           ))}
         </ul>
+        <div className="group relative my-2 inline-block overflow-hidden rounded-full bg-gray-500 shadow-md transition-all group-hover:rounded-md">
+          <Link href="/create">
+            <MaterialSymbolsLightAdd
+              width={40}
+              height={40}
+              className="h-12 w-12 cursor-pointer rounded-full transition-all group-hover:rounded-md"
+            />
+          </Link>
+        </div>
       </ColumnWrapper>
     </nav>
   );
