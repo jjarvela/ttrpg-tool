4d6c142d3ac0624f2adf772edc0869997f08a1c8
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-03-21 12:36:45 +0200

    Alter landing form style to encompass possible feedback cards and error displays

--- app/(auth)/welcome/_components/LandingForm.tsx
+++ app/(auth)/welcome/_components/LandingForm.tsx
@@ -7,7 +7,7 @@ import RegisterForm from "../../../_components/forms/RegisterForm";
 export default function LandingForm({ className }: { className?: string }) {
   const [isLogin, setIsLogin] = useState(false);
   return (
-    <div className="bg-color-dark h-[25rem] w-full border-t-[1px] border-black50 sm:w-[20rem] sm:border-l-[1px] sm:border-t-0">
+    <div className="bg-color-dark min-h-[26rem] w-full border-t-[1px] border-black50 pb-2 sm:w-[20rem] sm:border-l-[1px] sm:border-t-0">
       {isLogin ? (
         <LoginForm goToRegister={() => setIsLogin(false)} title />
       ) : (
