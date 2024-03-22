902549b59b635582fdd4c3196f4ed5baae671fa2
TerlfY <141029876+TerlfY@users.noreply.github.com>
2024-03-22 16:05:02 +0200

    implement conditional rendering of sidemenu

--- app/(auth)/welcome/page.tsx
+++ app/(auth)/welcome/page.tsx
@@ -1,17 +1,23 @@
 import Main from "../../_components/wrappers/PageMain";
 import RowWrapper from "../../_components/wrappers/RowWrapper";
+import Layout from "../../layout";
 import Hero from "./_components/Hero";
 import Introduction from "./_components/Introduction";
 import LandingForm from "./_components/LandingForm";
 
 export default function Landing() {
   return (
-    <Main>
-      <RowWrapper breakPoint="sm" className="w-full border-b border-b-black50">
-        <Hero />
-        <LandingForm />
-      </RowWrapper>
-      <Introduction></Introduction>
-    </Main>
+    <Layout includeSideMenu={false}>
+      <Main>
+        <RowWrapper
+          breakPoint="sm"
+          className="w-full border-b border-b-black50"
+        >
+          <Hero />
+          <LandingForm />
+        </RowWrapper>
+        <Introduction></Introduction>
+      </Main>
+    </Layout>
   );
 }
