ad561b6d0bce93c7015a6a0c2e8943e414b15fd8
TerlfY <141029876+TerlfY@users.noreply.github.com>
2024-03-28 12:31:35 +0200

    add hover and active styling

--- app/_components/TopMenu.tsx
+++ app/_components/TopMenu.tsx
@@ -1,46 +1,59 @@
-import Fa6SolidDiceD20 from "../../public/icons/Fa6SolidDiceD20";
+"use client";
+import Link from "next/link";
 import IconoirHexagonDice from "../../public/icons/IconoirHexagonDice";
 import MaterialSymbolsAndroidMessagesOutline from "../../public/icons/MaterialSymbolsAndroidMessagesOutline";
 import MaterialSymbolsCastleOutline from "../../public/icons/MaterialSymbolsCastleOutline";
 import MaterialSymbolsChessOutline from "../../public/icons/MaterialSymbolsChessOutline";
 import MaterialSymbolsGroupOutline from "../../public/icons/MaterialSymbolsGroupOutline";
-import MaterialSymbolsLightCastle from "../../public/icons/MaterialSymbolsLightCastle";
-import MaterialSymbolsLightChat from "../../public/icons/MaterialSymbolsLightChat";
-import MaterialSymbolsLightChess from "../../public/icons/MaterialSymbolsLightChess";
-import MaterialSymbolsLightNoteStack from "../../public/icons/MaterialSymbolsLightNoteStack";
-import MaterialSymbolsLightPersonPlay from "../../public/icons/MaterialSymbolsLightPersonPlay";
-import MaterialSymbolsLightSchedule from "../../public/icons/MaterialSymbolsLightSchedule";
 import MaterialSymbolsNoteStackOutline from "../../public/icons/MaterialSymbolsNoteStackOutline";
 import MaterialSymbolsPersonPlayOutline from "../../public/icons/MaterialSymbolsPersonPlayOutline";
 import MaterialSymbolsScheduleOutline from "../../public/icons/MaterialSymbolsScheduleOutline";
 import RiPushpinLine from "../../public/icons/RiPushpinLine";
+import { usePathname } from "next/navigation";
 
+interface IconLinkProps {
+  href: string;
+  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+}
+
+const IconLink: React.FC<IconLinkProps> = ({ href, icon: Icon }) => {
+  const pathname = usePathname();
+
+  return (
+    <Link href={href}>
+      <div
+        className={`group cursor-pointer ${pathname === href ? "text-green-500" : ""}`}
+      >
+        <Icon className="h-7 w-7 opacity-60 hover:opacity-100 active:stroke-primary" />
+      </div>
+    </Link>
+  );
+};
 const TopMenu = () => {
   return (
     <nav className="relative">
       <div className="bg-color-dark fixed flex w-full items-center justify-between px-6 py-4">
         <div className="ms-14 flex items-center space-x-4">
           {/* Left icons */}
-          <MaterialSymbolsCastleOutline className="h-7 w-7" />
-          <MaterialSymbolsAndroidMessagesOutline className="h-7 w-7" />
-          <MaterialSymbolsPersonPlayOutline className="h-7 w-7" />
-          <MaterialSymbolsChessOutline className="h-7 w-7" />
-          <MaterialSymbolsNoteStackOutline className="h-7 w-7" />
-          <IconoirHexagonDice className="h-7 w-7" />
-          <MaterialSymbolsScheduleOutline className="h-7 w-7" />
+          <IconLink href="/server" icon={MaterialSymbolsCastleOutline} />
+          <IconLink href="/chat" icon={MaterialSymbolsAndroidMessagesOutline} />
+          <IconLink
+            href="/characters"
+            icon={MaterialSymbolsPersonPlayOutline}
+          />
+          <IconLink href="/boards" icon={MaterialSymbolsChessOutline} />
+          <IconLink href="/notes" icon={MaterialSymbolsNoteStackOutline} />
+          <IconLink href="/dice" icon={IconoirHexagonDice} />
+          <IconLink href="/worldclock" icon={MaterialSymbolsScheduleOutline} />
         </div>
         <div className="flex items-center space-x-4">
           {/* Right icons */}
-          <RiPushpinLine className="h-7 w-7" />
-          <MaterialSymbolsGroupOutline className="h-7 w-7" />
+          <IconLink href="/pinned" icon={RiPushpinLine} />
+          <IconLink href="/friends" icon={MaterialSymbolsGroupOutline} />
         </div>
       </div>
     </nav>
   );
-};
-
-const Icon: React.FC = () => {
-  return <div className="h-8 w-8 rounded-full bg-gray-400"></div>;
 };
 
 export default TopMenu;
--- app/server/layout.tsx
+++ app/server/layout.tsx
@@ -1,4 +1,5 @@
 import SideMenu from "../_components/SideMenu";
+import TopMenu from "../_components/TopMenu";
 
 export default function ServerLayout({
   children,
@@ -7,7 +8,7 @@ export default function ServerLayout({
 }) {
   return (
     <div className="flex flex-col">
-      {/*prep for top menu*/}
+      <TopMenu />
       <div className="flex">
         <SideMenu />
         {children}
