a64a67d84eee2ebbb442c2eb3d9bea1382240aea
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-04-11 12:09:39 +0300

    Add profile info section and section separators to user preferences page

--- app/(user)/preferences/page.tsx
+++ app/(user)/preferences/page.tsx
@@ -4,21 +4,34 @@ import { auth } from "../../../auth";
 import { getUserById } from "../../../prisma/services/userService";
 import FeedbackCard from "../../_components/FeedbackCard";
 import Main from "../../_components/wrappers/PageMain";
+import ProfileInfo from "./_components/ProfileInfo";
+import Icon from "@/app/_components/Icon";
 
 export default async function UserPreferences() {
   const session = await auth();
 
-  if (!session)
-    return redirect("/welcome");
+  if (!session) return redirect("/welcome");
 
   const user = await getUserById((session as ExtendedSession).userId);
 
-  if (typeof user === "string")
+  if (!user || typeof user === "string")
     return <FeedbackCard type="error" message="Something went wrong!" />;
 
   return (
     <Main className="p-4">
-      <AccountInfo user={user || undefined} />
+      <AccountInfo user={user} />
+      <div className="mx-auto h-[1px] w-full bg-black50"></div>
+      <ProfileInfo
+        user={user}
+        profile_image={
+          <Icon
+            filename={user.profile_image || ""}
+            alt="profile image"
+            className="absolute left-0 top-0"
+          />
+        }
+      />
+      <div className="mx-auto h-[1px] w-full bg-black50"></div>
     </Main>
   );
 }
