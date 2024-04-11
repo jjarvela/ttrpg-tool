16430bf4b590be22c80290559dd2d7039c49f2fe
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-04-11 12:06:42 +0300

    Add optional className prop to Icon component

--- app/_components/Icon.tsx
+++ app/_components/Icon.tsx
@@ -1,18 +1,24 @@
 import getBlobSASUrl from "@/actions/getBlobSASUrl";
+import { twMerge } from "tailwind-merge";
 
 export default async function Icon({
   filename,
   alt,
+  className,
 }: {
   filename: string;
   alt: string;
+  className?: string;
 }) {
   const icon = await getBlobSASUrl(filename);
   return (
     <img
       src={icon}
       alt={alt}
-      className="min-h-[100%] min-w-[100%] rounded-full object-cover transition-all group-hover:rounded-md"
+      className={twMerge(
+        "min-h-[100%] min-w-[100%] rounded-full object-cover transition-all group-hover:rounded-md",
+        className,
+      )}
     />
   );
 }
