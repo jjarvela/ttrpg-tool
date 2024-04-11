cf60f854588be320a114362f5bc2298170535ae2
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-04-11 12:59:38 +0300

    add documentation to postUpload function

--- utils/postUpload.ts
+++ utils/postUpload.ts
@@ -1,5 +1,12 @@
 import axios, { AxiosResponse } from "axios";
 
+/**
+ * This helper function converts the file into base64 and sends it to the /api/upload route
+ * The function itself returns void, in order to access the axios response, use callback param
+ * @param file the file to be uploaded
+ * @param callback (response) => void // the response.data object comes back either as {status: number, message: error message} or {status: number, filename: filename of the uploaded file}
+ */
+
 export default function postUpload(
   file: File,
   callback: (response: AxiosResponse) => void,
