a57b6ad236edf609ff061e976b762fce02163520
TerlfY <141029876+TerlfY@users.noreply.github.com>
2024-04-12 15:40:08 +0300

    install dndkit, create sample note

---
+++ app/server/[serverId]/notes/note.tsx
@@ -0,0 +1,92 @@
+import React, { useCallback, useMemo, useState } from "react";
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { useDraggable } from "@dnd-kit/core";
+
+interface NoteProps {
+  id: string;
+  initialPosition: { x: number; y: number };
+  text: string;
+  onTextChange: (id: string, text: string) => void;
+}
+
+const Note: React.FC<NoteProps> = ({
+  id,
+  initialPosition,
+  text,
+  onTextChange,
+}) => {
+  const [position, setPosition] = useState(initialPosition);
+
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id,
+  });
+
+  const handleDragEnd = useCallback(
+    (event: React.MouseEvent<HTMLDivElement>) => {
+      // Update the position state with the new coordinates
+      setPosition({
+        x: position.x + event.movementX,
+        y: position.y + event.movementY,
+      });
+    },
+    [setPosition, position],
+  );
+
+  const handleTouchEnd = useCallback(
+    (event: React.TouchEvent<HTMLDivElement>) => {
+      setPosition({
+        x: position.x + event.changedTouches[0].clientX,
+        y: position.y + event.changedTouches[0].clientY,
+      });
+    },
+    [setPosition, position],
+  );
+
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: text,
+    onUpdate: ({ editor }) => {
+      onTextChange(id, editor.getHTML());
+    },
+  });
+
+  return useMemo(
+    () => (
+      <div
+        ref={setNodeRef}
+        {...attributes}
+        {...listeners}
+        style={{
+          position: "absolute",
+          left: position.x,
+          top: position.y,
+          transform: transform
+            ? `translate(${transform.x}px, ${transform.y}px)`
+            : "none",
+          border: "1px solid #ccc",
+          padding: "10px",
+          borderRadius: "5px",
+          backgroundColor: "grey",
+        }}
+        onMouseUp={handleDragEnd}
+        onTouchEnd={handleTouchEnd}
+      >
+        <EditorContent editor={editor} />
+      </div>
+    ),
+    [
+      setNodeRef,
+      attributes,
+      listeners,
+      position.x,
+      position.y,
+      transform,
+      handleDragEnd,
+      handleTouchEnd,
+      editor,
+    ],
+  );
+};
+
+export default Note;
--- app/server/[serverId]/notes/page.tsx
+++ app/server/[serverId]/notes/page.tsx
@@ -1,20 +1,32 @@
+"use client";
 import FeedbackCard from "@/app/_components/FeedbackCard";
 import Main from "@/app/_components/wrappers/PageMain";
 import { getServerData } from "@/prisma/services/serverService";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
+import Note from "./note";
+import { DndContext } from "@dnd-kit/core";
 
-export default async function ServerNotes({ params }: { params: Params }) {
+export default function ServerNotes({ params }: { params: Params }) {
   const id = params.serverId;
-
-  const server = await getServerData(id);
+  const server = getServerData(id);
 
   if (!server || typeof server === "string") {
     return <FeedbackCard type="error" message="Something went wrong." />;
   }
 
   return (
-    <Main className="mx-4">
-      <h1>Hewwo</h1>
-    </Main>
+    <DndContext>
+      <Main className="mx-4">
+        <h1>Hewwo</h1>
+        <Note
+          id="1"
+          initialPosition={{ x: 300, y: 200 }}
+          text={"JammaJuu"}
+          onTextChange={function (id: string, text: string): void {
+            throw new Error("Function not implemented.");
+          }}
+        />
+      </Main>
+    </DndContext>
   );
 }
