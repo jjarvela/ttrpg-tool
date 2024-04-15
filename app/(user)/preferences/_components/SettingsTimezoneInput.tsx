50a12fc383633fd188014bffb2d67971f246ce8a
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-04-15 10:39:37 +0300

    Add success feedback when user preferences are changed

--- app/(user)/preferences/_components/SettingsTimezoneInput.tsx
+++ app/(user)/preferences/_components/SettingsTimezoneInput.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import DropdownSelection from "@/app/_components/inputs/DropdownSelection";
+import MaterialSymbolsLightCheckCircleOutlineRounded from "@/public/icons/MaterialSymbolsLightCheckCircleOutlineRounded";
 import { validMomentTimezones } from "@/utils/timezones";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 type Option = { label: string; value: string | number | boolean };
 
@@ -14,34 +16,41 @@ export default function SettingsTimezoneInput({
   editInfo: (selected: Option[]) => void | Promise<string | undefined>;
 }) {
   const router = useRouter();
+  const [success, setSuccess] = useState(false);
 
   return (
-    <DropdownSelection
-      id="timezone"
-      defaultSelected={timezone ? [{ label: timezone, value: timezone }] : []}
-      className="w-full text-lg"
-      arrowClass="text-2xl py-[0.54rem] px-2"
-      startElement={
-        <label
-          htmlFor="timezone"
-          className="me-4 bg-black25 px-4 py-2 text-lg dark:bg-black75"
-        >
-          Timezone
-        </label>
-      }
-      options={validMomentTimezones.map((item) => {
-        return {
-          value: item,
-          label: item,
-        };
-      })}
-      onSelect={async (s) => {
-        const result = await editInfo(s);
-        if (result) {
-          return;
+    <div className="relative">
+      <DropdownSelection
+        id="timezone"
+        defaultSelected={timezone ? [{ label: timezone, value: timezone }] : []}
+        className="w-full text-lg"
+        arrowClass="text-2xl py-[0.54rem] px-2"
+        startElement={
+          <label
+            htmlFor="timezone"
+            className="me-4 bg-black25 px-4 py-2 text-lg dark:bg-black75"
+          >
+            Timezone
+          </label>
         }
-        router.refresh();
-      }}
-    />
+        options={validMomentTimezones.map((item) => {
+          return {
+            value: item,
+            label: item,
+          };
+        })}
+        onSelect={async (s) => {
+          const result = await editInfo(s);
+          if (result) {
+            return;
+          }
+          setSuccess(true);
+          router.refresh();
+        }}
+      />
+      {success && (
+        <MaterialSymbolsLightCheckCircleOutlineRounded className="absolute -right-10 top-4 text-2xl text-primary" />
+      )}
+    </div>
   );
 }
