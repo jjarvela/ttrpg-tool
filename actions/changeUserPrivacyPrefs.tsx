46b278cc66ec48f67d724acfdc1a0daa779ebbf3
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-04-15 11:20:18 +0300

    Add server action for changing user privacy prefs

---
+++ actions/changeUserPrivacyPrefs.tsx
@@ -0,0 +1,29 @@
+"use server";
+
+import { updateUser } from "@/prisma/services/userService";
+
+/**
+ * This function is set to update user data related to privacy and safety preferences
+ * @param id id of user to be updated
+ * @param data data to be updated (currently only share_timezone)
+ * @returns void on success, {error: string} on error
+ */
+
+export default async function changeUserPrivacyPrefs(
+  id: string,
+  data: {
+    //dm_permission?: "friends" | "server members" | "anyone";
+    //block_suspicious_links?: boolean;
+    //show_link_embeds: boolean;
+    share_timezone?: boolean;
+  },
+) {
+  if (!data) return { error: "No data to be updated" };
+
+  const updatedUser = await updateUser(id, data);
+
+  console.log(updatedUser);
+
+  if (typeof updatedUser === "string")
+    return { error: "Something went wrong. Please try again." };
+}
