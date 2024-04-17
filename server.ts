4c0bfea35bea9b45cc0e69099df8ce3a00610554
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-04-17 10:16:19 +0300

    Create custom nextJS server

---
+++ server.ts
@@ -0,0 +1,37 @@
+import { createServer } from "node:http";
+import next from "next";
+import { Server } from "socket.io";
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = 3000;
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port });
+const handler = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const httpServer = createServer(handler);
+
+  const io = new Server(httpServer);
+
+  io.on("connection", (socket) => {
+    console.log("connected to socket " + socket.id);
+
+    socket.on("add-user", (userId) => {
+      console.log("connection from " + userId);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("disconnected from socket " + socket.id);
+    });
+  });
+
+  httpServer
+    .once("error", (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
+});
