9e8a1c09edd9c657420a55c55e5d9bfa67ebba46
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-04-17 14:05:25 +0300

    add server-side events for direct messaging

--- server.ts
+++ server.ts
@@ -1,7 +1,11 @@
 import { createServer } from "node:http";
 import next from "next";
 import { Server } from "socket.io";
-import { findUserBySocket, updateUser } from "./prisma/services/userService";
+import {
+  findUserBySocket,
+  getUserById,
+  updateUser,
+} from "./prisma/services/userService";
 
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
@@ -21,16 +25,29 @@ app.prepare().then(() => {
     socket.on("send-user", (user_id: string) => {
       //when user has connected, add user's socket id to db (can be used to send events and check active status)
       updateUser(user_id, { socket_id: socket.id }).then((user) => {
-        console.log(user);
         return;
       });
     });
+
+    socket.on("send-message", async (recipient_id: string) => {
+      console.log("sending message to " + recipient_id);
+      const recipient = await getUserById(recipient_id, { socket_id: true });
+      console.log(recipient);
+      if (recipient && typeof recipient !== "string") {
+        //if recipient has a socket_id set, send event to their socket
+        recipient.socket_id &&
+          socket.to(recipient.socket_id).emit("received-message");
+      } else {
+        //if recipient returned error, send error event to sender's client
+        socket.emit("message-error", recipient || "Recipient does not exist");
+      }
+    });
+
     socket.on("disconnect", () => {
       //when user disconnects, return user's socket id to null
       findUserBySocket(socket.id).then((user) => {
         if (user && typeof user !== "string") {
           updateUser(user.id, { socket_id: null }).then((user) => {
-            console.log(user);
             return;
           });
         }
