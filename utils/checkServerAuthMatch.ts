a305f11389ca18d08b0f02cac7bc095ccd4211fd
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-04-19 16:10:05 +0300

    update checkAuthMatch with new global types

--- utils/checkServerAuthMatch.ts
+++ utils/checkServerAuthMatch.ts
@@ -1,21 +1,6 @@
 export default function checkAuthMatch(
-  serverAuth: {
-    id: number;
-    server_id: string;
-    member_id: string;
-    role: string;
-    nickname: string | null;
-    icon: string | null;
-  },
-  config: {
-    id: number;
-    server_id: string;
-    config_permission: string;
-    protected: boolean | null;
-    password_hash: string | null;
-    explorable: boolean | null;
-    searchable: boolean | null;
-  },
+  serverAuth: ServerAuth,
+  config: ServerConfig,
 ) {
   const permissions = config.config_permission;
   if (permissions === "All members") return true;
