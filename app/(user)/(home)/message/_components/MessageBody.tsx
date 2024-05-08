319c67c1442c83d9607620146e2a3db2d9070e3e
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-05-08 13:39:40 +0300

    Add timeout to clear private message notifications and Refresher to refresh the client after

--- app/(user)/(home)/message/_components/MessageBody.tsx
+++ app/(user)/(home)/message/_components/MessageBody.tsx
@@ -1,10 +1,14 @@
-import { getUnreadForUserConversation } from "@/prisma/services/notificationService";
+import {
+  deleteNotification,
+  getUnreadForUserConversation,
+} from "@/prisma/services/notificationService";
 import {
   getConversationByParticipants,
   getMessages,
 } from "../../../../../prisma/services/conversationService";
 import MessageCell from "./MessageCell";
 import ColumnWrapper from "@/app/_components/wrappers/ColumnWrapper";
+import { Refresher } from "@/app/_components/wrappers/Refresher";
 
 type MessageProps = {
   senderId: string;
@@ -24,9 +28,37 @@ export default async function MessageBody({
 
   const unread = await getUnreadForUserConversation(senderId, conversation.uid);
 
+  if (typeof unread !== "string" && unread.length > 0) {
+    setTimeout(() => {
+      console.log("timeout");
+      unread.forEach(async (notification) => {
+        await deleteNotification(notification.id);
+      });
+    }, 4000);
+  }
+
   const messages = await getMessages(conversation.uid);
   if (typeof messages !== "string") {
     const listMessages = messages.map((message) => {
+      if (
+        typeof unread !== "string" &&
+        unread.length > 0 &&
+        message.uid === unread[0].message_id
+      ) {
+        return (
+          <Refresher key={message.uid} array={unread}>
+            <div className="w-full border-t-[1px] border-primary text-center text-primary">
+              New messages
+            </div>
+            <MessageCell
+              key={message.uid}
+              sender_id={message.sender_id}
+              message={message.message}
+              created_at={message.created_at}
+            />
+          </Refresher>
+        );
+      }
       return (
         <MessageCell
           key={message.uid}
