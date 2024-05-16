12ce5360f19b4f465178e720e6c1f7fd50ff9345
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-05-16 14:45:56 +0300

    Handle server not found in ServerJoinPage

--- app/(user)/server/[serverId]/_components/serverJoinComponents/ServerJoinPage.tsx
+++ app/(user)/server/[serverId]/_components/serverJoinComponents/ServerJoinPage.tsx
@@ -2,63 +2,64 @@ import FeedbackCard from "@/app/_components/FeedbackCard";
 import { getServerJoinData } from "@/prisma/services/serverService";
 import NotOpen from "./NotOpen";
 import Open from "./Open";
+import ServerNotFound from "../ServerNotFound";
 
 export default async function ServerJoinPage({
   server_id,
 }: {
   server_id: string;
 }) {
-  const server = await getServerJoinData(server_id);
+  try {
+    const server = await getServerJoinData(server_id);
+    if (
+      !server.config[0].join_permission ||
+      server.config[0].join_permission === "invitation link"
+    )
+      return (
+        <NotOpen reason="Unfortunately this server can only be joined by a direct invitation." />
+      );
 
-  if (!server || typeof server === "string")
-    return <FeedbackCard type="error" message="Something went wrong" />;
-
-  if (
-    !server.config[0].join_permission ||
-    server.config[0].join_permission === "invitation link"
-  )
-    return (
-      <NotOpen reason="Unfortunately this server can only be joined by a direct invitation." />
+    const validUnlimited = server.invitations.filter(
+      (item) => !item.max_uses && new Date(item.expires) > new Date(),
     );
 
-  const validUnlimited = server.invitations.filter(
-    (item) => !item.max_uses && new Date(item.expires) > new Date(),
-  );
+    if (server.config[0].join_permission === "unlimited invitation")
+      if (validUnlimited.length === 0)
+        return (
+          <NotOpen reason="Unfortunately this server has no open invitations." />
+        );
+      else {
+        return (
+          <Open
+            server_name={server.server_name}
+            needsPassword={server.config[0].protected}
+            invitation_id={validUnlimited[0].id}
+          />
+        );
+      }
 
-  if (server.config[0].join_permission === "unlimited invitation")
-    if (validUnlimited.length === 0)
+    const validLimited = server.invitations.filter(
+      (item) =>
+        item.max_uses &&
+        new Date(item.expires) > new Date() &&
+        item.used_count < item.max_uses,
+    );
+
+    if (validUnlimited.length === 0 && validLimited.length === 0)
       return (
         <NotOpen reason="Unfortunately this server has no open invitations." />
       );
-    else {
-      return (
-        <Open
-          server_name={server.server_name}
-          needsPassword={server.config[0].protected}
-          invitation_id={validUnlimited[0].id}
-        />
-      );
-    }
 
-  const validLimited = server.invitations.filter(
-    (item) =>
-      item.max_uses &&
-      new Date(item.expires) > new Date() &&
-      item.used_count < item.max_uses,
-  );
-
-  if (validUnlimited.length === 0 && validLimited.length === 0)
     return (
-      <NotOpen reason="Unfortunately this server has no open invitations." />
+      <Open
+        server_name={server.server_name}
+        needsPassword={server.config[0].protected}
+        invitation_id={
+          validUnlimited[0] ? validUnlimited[0].id : validLimited[0].id
+        }
+      />
     );
-
-  return (
-    <Open
-      server_name={server.server_name}
-      needsPassword={server.config[0].protected}
-      invitation_id={
-        validUnlimited[0] ? validUnlimited[0].id : validLimited[0].id
-      }
-    />
-  );
+  } catch (e) {
+    return <ServerNotFound />;
+  }
 }
