405c4c497a4e7ba0726376fd4122df5ac3f737a0
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-05-17 12:49:46 +0300

    Update TopMenu import

--- app/(user)/server/[serverId]/_components/LayoutClientWrapper.tsx
+++ app/(user)/server/[serverId]/_components/LayoutClientWrapper.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import TopMenu from "@/app/_components/TopMenu";
+import TopMenu from "@/app/(user)/server/[serverId]/_components/TopMenu";
 import ColumnWrapper from "@/app/_components/wrappers/ColumnWrapper";
 import RowWrapper from "@/app/_components/wrappers/RowWrapper";
 import MaterialSymbolsGroupOutline from "@/public/icons/MaterialSymbolsGroupOutline";
@@ -8,16 +8,42 @@ import MaterialSymbolsLightChevronLeftRounded from "@/public/icons/MaterialSymbo
 import { ReactNode, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
-export default function LayoutClientWrapper({id, innerNav, membersMenu, children} : {id: string, innerNav: ReactNode, membersMenu: ReactNode, children: ReactNode}) {
+export default function LayoutClientWrapper({
+  id,
+  innerNav,
+  membersMenu,
+  children,
+}: {
+  id: string;
+  innerNav: ReactNode;
+  membersMenu: ReactNode;
+  children: ReactNode;
+}) {
   const [showInnerNav, setShowInnerNav] = useState(true);
   const [showMembers, setShowMembers] = useState(true);
-  return (<div className="flex flex-grow">
+  return (
+    <div className="flex flex-grow">
       {/*server inner nav*/}
-      <RowWrapper className={twMerge("gap-0 md:max-w-[15%]", showInnerNav ? "w-40" : "")}>
-      <div className={twMerge("h-full w-full md:block", showInnerNav ? "block" : "hidden")}>{innerNav}</div>
-      <div className="flex flex-col rounded-r-lg bg-black50 md:hidden cursor-pointer text-lg h-[5%]">
-        <MaterialSymbolsLightChevronLeftRounded className={twMerge("my-auto justify-self-center ease-linear duration-300", showInnerNav ? "" : "rotate-180")} onClick={() => setShowInnerNav(prev => !prev)}/>
-      </div>
+      <RowWrapper
+        className={twMerge("gap-0 md:max-w-[15%]", showInnerNav ? "w-40" : "")}
+      >
+        <div
+          className={twMerge(
+            "h-full w-full md:block",
+            showInnerNav ? "block" : "hidden",
+          )}
+        >
+          {innerNav}
+        </div>
+        <div className="flex h-[5%] cursor-pointer flex-col rounded-r-lg bg-black50 text-lg md:hidden">
+          <MaterialSymbolsLightChevronLeftRounded
+            className={twMerge(
+              "my-auto justify-self-center duration-300 ease-linear",
+              showInnerNav ? "" : "rotate-180",
+            )}
+            onClick={() => setShowInnerNav((prev) => !prev)}
+          />
+        </div>
       </RowWrapper>
 
       {/*top menu and content*/}
@@ -25,14 +51,18 @@ export default function LayoutClientWrapper({id, innerNav, membersMenu, children
         align="items-start"
         className="m-0 flex-grow gap-0 p-0 lg:gap-2"
       >
-        <TopMenu serverId={id} setShowMembers={setShowMembers}/>
+        <TopMenu serverId={id} setShowMembers={setShowMembers} />
         {children}
       </ColumnWrapper>
 
       {/*server members bar*/}
-      <RowWrapper className="z-[999] lg:z-0 h-full bg-color-dark border-l-[1px] border-black50 pl-2 lg:pl-0 absolute top-0 right-0 lg:relative">
-        <MaterialSymbolsGroupOutline className="h-7 w-7 cursor-pointer opacity-60 hover:opacity-100 block lg:hidden self-start justify-self-center" onClick={() => setShowMembers(prev => !prev)}/>
+      <RowWrapper className="bg-color-dark absolute right-0 top-0 z-[999] h-full border-l-[1px] border-black50 pl-2 lg:relative lg:z-0 lg:pl-0">
+        <MaterialSymbolsGroupOutline
+          className="block h-7 w-7 cursor-pointer self-start justify-self-center opacity-60 hover:opacity-100 lg:hidden"
+          onClick={() => setShowMembers((prev) => !prev)}
+        />
         {showMembers && membersMenu}
       </RowWrapper>
-    </div>)
+    </div>
+  );
 }
