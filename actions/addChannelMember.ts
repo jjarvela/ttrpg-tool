49e4957cdbb86aed240dc34e19ac26107c4c1ef5
AnniVirolainen <annimaria.virolainen@gmail.com>
2024-05-17 15:37:16 +0300

    errorHandler: give parameters as arrow functions

--- actions/addChannelMember.ts
+++ actions/addChannelMember.ts
@@ -9,12 +9,21 @@ export default async function addChannelMember(
   users: string[],
   channelId: string,
 ) {
-  const conversation = await errorHandler(getConversationByChannelId, [
-    channelId,
-  ]);
+  const conversation = await errorHandler(
+    async () => {
+      const conversation = await getConversationByChannelId(channelId);
+      return conversation;
+    },
+    () => {
+      return null;
+    },
+  );
+
   if (conversation) {
     const conversationId = conversation.uid;
 
-    return errorHandler(addChannelConversationMember, [conversationId, users]);
+    return errorHandler(async () => {
+      await addChannelConversationMember(conversationId, users);
+    });
   } else return { error: "Something went wrong. Please try again." };
 }
--- utils/errorHandler.ts
+++ utils/errorHandler.ts
@@ -1,16 +1,12 @@
 export default async function errorHandler(
-  this: any,
   func: Function,
-  args1: Array<any>,
   exceptionFunc?: Function,
-  args2?: Array<any>,
 ) {
   try {
-    return func.apply(this, args1);
+    return func();
   } catch (e) {
     if (exceptionFunc) {
-      exceptionFunc.apply(this, args2);
+      exceptionFunc();
     }
-    throw new Error((e as Error).message);
   }
 }
