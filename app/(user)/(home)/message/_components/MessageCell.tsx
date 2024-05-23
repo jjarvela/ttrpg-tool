6fc4768d5e7099e1c80c3e5cb2ebbc82183daa6d
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-05-23 10:38:14 +0300

    Update error handling in message cell

--- app/(user)/(home)/message/_components/MessageCell.tsx
+++ app/(user)/(home)/message/_components/MessageCell.tsx
@@ -2,6 +2,7 @@ import ColumnWrapper from "../../../../_components/wrappers/ColumnWrapper";
 import RowWrapper from "../../../../_components/wrappers/RowWrapper";
 import UserInfo from "@/app/_components/UserInfo";
 import { getUserById } from "../../../../../prisma/services/userService";
+import errorHandler from "@/utils/errorHandler";
 
 type MessageCellProps = {
   sender_id: string;
@@ -14,31 +15,41 @@ export default async function MessageCell({
   message,
   created_at,
 }: MessageCellProps) {
-  const userInfo = await getUserById(sender_id, {
-    username: true,
-    screen_name: true,
-    profile_image: true,
-  });
-  if (userInfo && typeof userInfo !== "string") {
-    const time = created_at.toLocaleString().slice(0, -3);
-    return (
-      <div className="hover:bg-primary-soft w-full border-t p-2 dark:hover:bg-black50">
-        <RowWrapper justify="justify-start justify-items-start">
-          <UserInfo
-            key={userInfo.id}
-            className="w-[max-content]"
-            username={userInfo.username}
-            width={40}
-            image={userInfo.profile_image ? userInfo.profile_image : undefined}
-            isActive={false}
-            screen_name={userInfo.screen_name || undefined}
-          />
-          <p className="text-sm">{time}</p>
-        </RowWrapper>
-        <div className="text-left">
-          <p className="ms-10">{message}</p>
-        </div>
+  const element: JSX.Element | null = await errorHandler(
+    async () => {
+      const userInfo = await getUserById(sender_id, {
+        username: true,
+        screen_name: true,
+        profile_image: true,
+      });
+
+      return (
+        <UserInfo
+          key={userInfo.id}
+          className="w-[max-content]"
+          username={userInfo.username}
+          width={40}
+          image={userInfo.profile_image ? userInfo.profile_image : undefined}
+          isActive={false}
+          screen_name={userInfo.screen_name || undefined}
+        />
+      );
+    },
+    () => {
+      return null;
+    },
+  );
+
+  const time = created_at.toLocaleString().slice(0, -3);
+  return (
+    <div className="hover:bg-primary-soft w-full border-t p-2 dark:hover:bg-black50">
+      <RowWrapper justify="justify-start justify-items-start">
+        {element && element}
+        <p className="text-sm">{time}</p>
+      </RowWrapper>
+      <div className="text-left">
+        <p className="ms-10">{message}</p>
       </div>
-    );
-  }
+    </div>
+  );
 }
