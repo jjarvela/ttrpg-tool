e3ab1da4c533026324a25ac776c6bc492ea1fa03
TerlfY <141029876+TerlfY@users.noreply.github.com>
2024-05-28 14:05:54 +0300

    add createdAt & updatedAt to schema, slice 6 from notes

--- app/(user)/server/[serverId]/home/_components/LatestNotes.tsx
+++ app/(user)/server/[serverId]/home/_components/LatestNotes.tsx
@@ -95,7 +95,7 @@ export default function LatestNotes() {
         </h2>
       </div>
       <div className="m-4 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
-        {notes.map((note) => (
+        {notes.slice(0, 6).map((note) => (
           <HomeNote
             note={note}
             currentUser={{
