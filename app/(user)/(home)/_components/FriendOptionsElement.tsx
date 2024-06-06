f19f662927a76b4215764798b65c5d36c50f6cac
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-06-06 09:56:25 +0300

    Put unfriend and block in submenu on FriendOptionsElement

--- app/(user)/(home)/_components/FriendOptionsElement.tsx
+++ app/(user)/(home)/_components/FriendOptionsElement.tsx
@@ -3,12 +3,17 @@
 import blockUser from "@/actions/userRelationshipManagement/blockUser";
 import unfriendUser from "@/actions/userRelationshipManagement/unfriendUser";
 import ConfirmModal from "@/app/_components/ConfirmModal";
+import ColumnWrapper from "@/app/_components/wrappers/ColumnWrapper";
 import RowWrapper from "@/app/_components/wrappers/RowWrapper";
+import MaterialSymbolsAndroidMessagesOutline from "@/public/icons/MaterialSymbolsAndroidMessagesOutline";
 import MaterialSymbolsBlock from "@/public/icons/MaterialSymbolsBlock";
 import MaterialSymbolsPersonCancelOutlineRounded from "@/public/icons/MaterialSymbolsPersonCancelOutlineRounded";
 import MaterialSymbolsPersonRemoveOutlineRounded from "@/public/icons/MaterialSymbolsPersonRemoveOutlineRounded";
+import SolarMenuDotsBold from "@/public/icons/SolarMenuDotsBold";
 import { friendRequestEvent } from "@/socket";
 import errorHandler from "@/utils/errorHandler";
+import handleClickOutside from "@/utils/handleClickOutside";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { Fragment, useRef, useState, useTransition } from "react";
 import { twMerge } from "tailwind-merge";
@@ -24,6 +29,8 @@ export default function FriendOptionsElement({
   const [error, setError] = useState("");
   const [unfriendSuccess, setUnfriendSuccess] = useState(false);
   const [blockSuccess, setBlockSuccess] = useState(false);
+
+  const [isOpen, setIsOpen] = useState(false);
 
   const router = useRouter();
 
@@ -65,28 +72,55 @@ export default function FriendOptionsElement({
 
   const confirmRemoveRef = useRef<HTMLDialogElement | null>(null);
   const confirmBlockRef = useRef<HTMLDialogElement | null>(null);
+  const menuRef = useRef<HTMLDivElement | null>(null);
   return (
     <Fragment>
       <RowWrapper
         align="items-center"
         justify="justify-items-center"
-        className="px-2 sm:flex-col"
+        className="relative px-2 sm:flex-col"
       >
-        {unfriendSuccess ? (
-          <MaterialSymbolsPersonCancelOutlineRounded className="flex-shrink-0 cursor-pointer text-2xl text-warning" />
-        ) : (
-          <MaterialSymbolsPersonRemoveOutlineRounded
+        <Link href={`/message/${user_id}`}>
+          <MaterialSymbolsAndroidMessagesOutline className="flex-shrink-0 text-2xl" />
+        </Link>
+        <div id="hitbox" className="relative" ref={menuRef}>
+          <SolarMenuDotsBold
             className="flex-shrink-0 cursor-pointer text-2xl"
-            onClick={() => confirmRemoveRef.current?.showModal()}
+            onClick={() => {
+              document.addEventListener("mousedown", (event) =>
+                handleClickOutside(menuRef, event, () => setIsOpen(false)),
+              );
+              setIsOpen((prev) => !prev);
+            }}
           />
-        )}
-        <MaterialSymbolsBlock
-          className={twMerge(
-            "mr-[0.4rem] flex-shrink-0 cursor-pointer text-2xl",
-            blockSuccess && "text-warning",
+          {isOpen && (
+            <ColumnWrapper className="bg-color-default absolute left-[2rem] top-[1.5rem] rounded-lg border-[1px] border-black50">
+              <RowWrapper
+                className="cursor-pointer"
+                onClick={() => confirmRemoveRef.current?.showModal()}
+              >
+                {unfriendSuccess ? (
+                  <MaterialSymbolsPersonCancelOutlineRounded className="flex-shrink-0 text-2xl text-warning" />
+                ) : (
+                  <MaterialSymbolsPersonRemoveOutlineRounded className="flex-shrink-0 text-2xl" />
+                )}
+                <p>Unfriend</p>
+              </RowWrapper>
+              <RowWrapper
+                className="cursor-pointer"
+                onClick={() => confirmBlockRef.current?.showModal()}
+              >
+                <MaterialSymbolsBlock
+                  className={twMerge(
+                    "mr-[0.4rem] flex-shrink-0 text-2xl",
+                    blockSuccess && "text-warning",
+                  )}
+                />
+                <p>Block</p>
+              </RowWrapper>
+            </ColumnWrapper>
           )}
-          onClick={() => confirmBlockRef.current?.showModal()}
-        />
+        </div>
       </RowWrapper>
       <ConfirmModal
         refObject={confirmRemoveRef}
