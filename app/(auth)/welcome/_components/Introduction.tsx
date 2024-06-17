a3ad4cdfc45c068e3bba95029d33c59d390650f3
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-06-17 13:30:49 +0300

    Add items to welcome page

--- app/(auth)/welcome/_components/Introduction.tsx
+++ app/(auth)/welcome/_components/Introduction.tsx
@@ -1,12 +1,46 @@
+import RowWrapper from "@/app/_components/wrappers/RowWrapper";
 import ColumnWrapper from "../../../_components/wrappers/ColumnWrapper";
+import getBlobSASUrl from "@/actions/getBlobSASUrl";
 
-export default function Introduction() {
+export default async function Introduction() {
+  const charactersImgUrl = await getBlobSASUrl("1718619538979.png");
+  const boardsImgUrl = await getBlobSASUrl("1718619560188.png");
+
   return (
     <section id="introduction-container" className="w-full">
-      <ColumnWrapper>
+      <ColumnWrapper className="w-full">
         <h2 className="text-center">
           Welcome to TTRPG-tool, the go-to tool for bringing the tabletop online
         </h2>
+        <RowWrapper
+          justify="md:justify-center"
+          breakPoint="md"
+          className="gap-8"
+        >
+          <img width={500} src={charactersImgUrl} alt="Create characters" />
+          <ColumnWrapper className="items-start md:w-[50%]">
+            <h3>Create and share your characters</h3>
+            <p>
+              With the server-based system, your character can live multiple
+              different lives simultaneously!
+            </p>
+          </ColumnWrapper>
+        </RowWrapper>
+
+        <RowWrapper
+          justify="md:justify-center"
+          breakPoint="md"
+          className="flex-col-reverse gap-8"
+        >
+          <ColumnWrapper className="items-start md:w-[50%]">
+            <h3>Explore your worlds</h3>
+            <p>
+              You can upload your own game boards to customise your story. Enjoy
+              real-time updates from all your party members!
+            </p>
+          </ColumnWrapper>
+          <img width={500} src={boardsImgUrl} alt="Explore" />
+        </RowWrapper>
       </ColumnWrapper>
     </section>
   );
