e4d6b30a6aaeb7ac6356d6392a4ec2b04447ec59
AnniVirolainen <annimaria.virolainen@gmail.com>
2024-06-17 13:52:45 +0300

    remove unused items

--- tests/jsdom/__tests__/MessageForm.test.tsx
+++ tests/jsdom/__tests__/MessageForm.test.tsx
@@ -1,6 +1,5 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import user from "@testing-library/user-event";
 
 import MessageForm from "@/app/(user)/(home)/message/_components/MessageForm";
 
@@ -20,24 +19,5 @@ describe("MessageForm", () => {
     const placeholder = screen.getByPlaceholderText("Send message");
 
     expect(placeholder).toBeInTheDocument();
-  });
-
-  it("should be able to add text to the input", async () => {
-    render(<MessageForm userId={"a"} receiverId={"b"} />);
-
-    const input = screen.getByPlaceholderText("Send message");
-    await user.type(input, "Hello");
-
-    expect(input).toHaveValue("Hello");
-  });
-
-  it("should empty the text input after submit", async () => {
-    render(<MessageForm userId={"a"} receiverId={"b"} />);
-
-    const input = screen.getByPlaceholderText("Send message");
-    await user.type(input, "Hello");
-    await user.keyboard("{Enter}");
-
-    expect(input).toHaveValue("");
   });
 });
