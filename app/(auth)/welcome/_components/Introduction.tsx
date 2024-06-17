f7c9ef5dc96e7cee30fbc1d76f0611ab68c16aed
Ltiilikainen <laura.tiilikainen@hotmail.com>
2024-06-17 13:58:51 +0300

    get images from public folder

--- app/(auth)/welcome/_components/Introduction.tsx
+++ app/(auth)/welcome/_components/Introduction.tsx
@@ -3,9 +3,6 @@ import ColumnWrapper from "../../../_components/wrappers/ColumnWrapper";
 import getBlobSASUrl from "@/actions/getBlobSASUrl";
 
 export default async function Introduction() {
-  const charactersImgUrl = await getBlobSASUrl("1718619538979.png");
-  const boardsImgUrl = await getBlobSASUrl("1718619560188.png");
-
   return (
     <section id="introduction-container" className="w-full">
       <ColumnWrapper className="w-full">
@@ -17,7 +14,7 @@ export default async function Introduction() {
           breakPoint="md"
           className="gap-8"
         >
-          <img width={500} src={charactersImgUrl} alt="Create characters" />
+          <img width={500} src={"/characters.png"} alt="Create characters" />
           <ColumnWrapper className="items-start md:w-[50%]">
             <h3>Create and share your characters</h3>
             <p>
@@ -39,7 +36,7 @@ export default async function Introduction() {
               real-time updates from all your party members!
             </p>
           </ColumnWrapper>
-          <img width={500} src={boardsImgUrl} alt="Explore" />
+          <img width={500} src={"/board.png"} alt="Explore" />
         </RowWrapper>
       </ColumnWrapper>
     </section>
