f1395378285d0b2b47b1c231bf60286c448b0088
TerlfY <141029876+TerlfY@users.noreply.github.com>
2024-06-17 13:48:59 +0300

    map latest messages into ChannelThumb

--- app/(user)/server/[serverId]/chat/_components/ChannelThumb.tsx
+++ app/(user)/server/[serverId]/chat/_components/ChannelThumb.tsx
@@ -1,14 +1,39 @@
 "use client";
 
-import { Channel } from "@prisma/client";
+import { Channel, Message } from "@prisma/client";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { twMerge } from "tailwind-merge";
+import handleGetNewMessages from "./GetNewMessagesHandler";
 
 export default function ChannelThumb({ channel }: { channel: Channel }) {
   const [isHovering, setIsHovering] = useState(false);
+  const [chatMessages, setChatMessages] = useState<Message[]>([]);
+  const messagesEndRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {});
+  const scrollToBottom = () => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  useEffect(() => {
+    const getChatMessages = async () => {
+      try {
+        const messages = await handleGetNewMessages(channel.uid);
+
+        setChatMessages(messages as Message[]);
+      } catch (error) {
+        console.error("Failed to fetch chat messages:", error);
+      }
+    };
+
+    getChatMessages();
+  }, [channel.uid]);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [chatMessages]);
 
   return (
     <Link
@@ -23,18 +48,34 @@ export default function ChannelThumb({ channel }: { channel: Channel }) {
       {channel && (
         <div
           className={twMerge(
-            "opacity-45 duration-200 ease-linear",
-            isHovering && "opacity-100",
+            "flex flex-col justify-end opacity-45 duration-200 ease-linear",
+            isHovering && "opacity-75",
           )}
           style={{
             position: "absolute",
-            minHeight: "100%",
-            minWidth: "100%",
-            objectFit: "cover",
+            top: 0,
+            bottom: 10,
+            left: 0,
+            right: 0,
+            overflowY: "auto",
+            padding: "10px",
           }}
-        />
+        >
+          <div className="m-2">
+            {chatMessages.map((message) => (
+              <div className="" key={message.uid}>
+                <p>
+                  {message.created_at.toLocaleTimeString()} {message.message}
+                </p>
+              </div>
+            ))}
+            <div ref={messagesEndRef} />
+          </div>
+        </div>
       )}
-      <h4 className="absolute bottom-0 right-2">{channel.channel_name}</h4>
+      <div>
+        <h4 className="absolute bottom-0 right-2">{channel.channel_name}</h4>
+      </div>
     </Link>
   );
 }
